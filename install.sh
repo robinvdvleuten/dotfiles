0014d098140d7dafd4c0305ecb74361d74b2e38e
Robin van der Vleuten <r.vandervleuten@sping.nl>
2019-05-01 09:52:44 +0200

    Install latest version of ruby and nodejs

--- install.sh
+++ install.sh
@@ -24,6 +24,27 @@ if [ ! -d "$HOME/.asdf" ]; then
   asdf plugin-add "nodejs" "https://github.com/asdf-vm/asdf-nodejs.git"
 fi
 
+install_asdf_language() {
+  local language="$1"
+  local version
+  version="$(asdf list-all "$language" | grep -v "[a-z]" | tail -1)"
+
+  if ! asdf list "$language" | grep -Fq "$version"; then
+    asdf install "$language" "$version"
+    asdf global "$language" "$version"
+  fi
+}
+
+echo "Installing latest Ruby ..."
+install_asdf_language "ruby"
+gem update --system
+number_of_cores=$(sysctl -n hw.ncpu)
+bundle config --global jobs $((number_of_cores - 1))
+
+echo "Installing latest Node ..."
+bash "$HOME/.asdf/plugins/nodejs/bin/import-release-team-keyring"
+install_asdf_language "nodejs"
+
 echo "Installing a few global NPM packages"
 npm install --global npm pure-prompt
 
